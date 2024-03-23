2455b3fdf00845d255ed00900e9f6d12ecdad540
laex0727 <khangdao2112@gmail.com>
2024-03-23 16:37:02 +0700

    Finished Tuan1 Bai1

--- src/main/java/oop/tuan1/Bai1.java
+++ src/main/java/oop/tuan1/Bai1.java
@@ -1,4 +1,9 @@
 package oop.tuan1;
 
 public class Bai1 {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Khang");
+        System.out.println("I'm 20 years old");
+        System.out.println("This is my first Java program");
+    }
 }
